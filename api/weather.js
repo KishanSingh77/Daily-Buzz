df46828a1d891bba94e1571f06328b11f2c80988
kishansingh77 <vivekthejoker777@gmail.com>
2019-09-29 14:10:51 -0400

    added user Count

--- api/weather.js
+++ api/weather.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const { apiKey } = require("../secrets/cred");
 const url = "http://api.openweathermap.org/data/2.5/weather";
 const request = require("request");
+let userCount = 0;
 
 router.get("/:location", (req, res, next) => {
+  userCount++;
   const location = req.params.location;
 
   request({
     uri: url + `?q=${location}&APPID=${apiKey}`
-  }).pipe(res);
+  }).pipe("Welcome user" + userCount + res);
 });
 
 module.exports = router;
