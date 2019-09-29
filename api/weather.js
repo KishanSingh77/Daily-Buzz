4b4b58071302c742d54c3c18a7fc5fda869c47ee
kishansingh77 <vivekthejoker777@gmail.com>
2019-09-29 15:54:24 -0400

    added user location query

--- api/weather.js
+++ api/weather.js
@@ -10,24 +10,42 @@ router.get("/:location", (req, res, next) => {
   userCount++;
   const location = req.params.location;
   places.push(location);
-  console.log(userCount);
+  const urlQuery = url + `?q=${location}&APPID=${apiKey}`;
 
-  request({
-    uri: url + `?q=${location}&APPID=${apiKey}`
-  }).pipe(res);
+  // request({
+  //   uri: urlQuery
+  // }).pipe(res);
+
+  request({ url: urlQuery }, (error, response, body) => {
+    // body = JSON.parse(body);
+    // console.log(body);
+
+    // const coordinate =
+    //   JSON.stringify(body.coord.lon) + " " + JSON.stringify(body.coord.lat);
+    // const weather = JSON.stringify(body.weather[0].description);
+    // const temperature = JSON.stringify(body.main.temp);
+    // const humidity = JSON.stringify(body.main.humidity);
+    // const pressure = JSON.stringify(body.main.pressure);
+    // const temp_min = JSON.stringify(body.main.temp_min);
+    // const temp_max = JSON.stringify(body.main.temp_max);
+    // const visibility = JSON.stringify(body.visibility);
+    // const windSpeed = JSON.stringify(body.wind.speed);
+    // const cloud = JSON.stringify(body.clouds.all);
+    // const sunrise = JSON.stringify(body.sys.sunrise);
+    // const sunset = JSON.stringify(body.sys.sunset);
+    // const timezone = JSON.stringify(body.timezone);
+
+    // let output = "<h3>" + coordinate + "<br/>" + weather + "</h3>";
+    // res.send(output);
+    res.send(body);
+  });
 });
 
 router.get("/users/places", (req, res, next) => {
-  console.log(userCount);
-  console.log(places);
-
   res.send("<h1>People checked for " + places.toString() + "</h1>");
 });
 
 router.get("/users/count", (req, res, next) => {
-  console.log(userCount);
-  console.log(places);
-
   res.send("<h1>People count " + userCount + "</h1>");
 });
 
