235d67045842d9dc30f9767813fe194c5be31b1f
kishansingh77 <vivekthejoker777@gmail.com>
2019-09-29 13:53:28 -0400

    changed port to process.env.PORT

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const weatherRoute = require("./api/weather");
-
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("home");
 });
 
 app.use("/weather", weatherRoute);
 
-app.listen("3000", () => {
+app.listen(`${port}`, () => {
   console.log("Server running");
 });
